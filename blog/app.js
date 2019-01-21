802524a4b58856dd04cb116b1118e761d3ac8513
chengyu <chengyuonline@163.com>
2019-01-21 15:20:42 +0800

    修改了app.js

--- blog/app.js
+++ blog/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 app.use(express.static(__dirname+'/html'));
-
+app.use('/res',express.static('./res'));
 app.listen(3000,res=>{
     console.log('server is running!')
 })
