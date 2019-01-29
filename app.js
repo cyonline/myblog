f91766a7e3941ea6b8ba9f194bcae899674e1b26
chengyu <chengyuonline@163.com>
2019-01-29 16:27:14 +0800

    将app.js移到外面

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+app.use(express.static(__dirname+'/dist'));
+app.use('/dist/assets', express.static('./dist/assets'));
+
+app.listen(4200,res=>{
+    console.log('server is running!')
+})
