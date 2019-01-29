e3214b491ecf36a7bb0f017742a6a50b28d18de9
chengyu <chengyuonline@163.com>
2019-01-29 16:37:09 +0800

    创建了server文件夹

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
-app.use(express.static(__dirname+'/dist'));
-app.use('/dist/assets', express.static('./dist/assets'));
+app.use(express.static(__dirname+'../dist'));
+app.use('../dist/assets', express.static('../dist/assets'));
 
 app.listen(4200,res=>{
     console.log('server is running!')
